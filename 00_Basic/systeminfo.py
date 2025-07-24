80028d52b9ce3d6fe243eeb1a69b26a18388ee29
Pallab Panda <pallab.panda@gmail.com>
2025-07-25 02:43:34 +0530

    update

---
+++ 00_Basic/systeminfo.py
@@ -0,0 +1,12 @@
+import sys
+print("Python version:", sys.version)
+print("Platform:", sys.platform)
+print("Executable:", sys.executable)
+print("Byte order:", sys.byteorder)
+print("Max size of an integer:", sys.maxsize)
+print("Path:", sys.path)
+print("Version info:", sys.version_info)
+print("API version:", sys.api_version)
+print("Flags:", sys.flags)
+print("Hex version:", sys.hexversion)
+print("Copyright:", sys.copyright)  
