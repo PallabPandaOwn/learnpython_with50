3bf3c91f5f248d9ad07274b2ce9d861fcf0d65f3
Pallab Panda <pallab.panda@gmail.com>
2025-08-20 11:35:56 +0530

    updated

--- 02_datatypes/chapter_1.py
+++ 02_datatypes/chapter_1.py
@@ -1,9 +1,26 @@
 sugar_amount = 2
-print("I have", {sugar_amount}, "cups of sugar.")
+print(f"I have {sugar_amount} cups of sugar.")
+print(f"ID of sugar_amount: {id(sugar_amount)}")
 
-mix = set(["sugar", "flour", "eggs"])
-print(f"I will mix:", {mix})
+sugar_amount = 3
+print(f"I have {sugar_amount} cups of sugar.")
+print(f"ID of sugar_amount: {id(sugar_amount)}")
 
-spices = set(Ginger,Cardamom)
 
+# flour_amount = 3
+# print(f"I have {flour_amount} cups of flour.")
+# print(f"ID of flour_amount: {id(flour_amount)}")
 
+# mix = set(["sugar", "flour", "eggs"])
+
+# print(f"I will mix:, {mix}")
+
+# print(f"ID of mix:", id(mix))
+
+spices={"Ginger","Cardamom","Cinnamon"}
+print(f"ID of spices:", id(spices))
+print(f"I will add spices:, {spices}")
+
+spices.add("Nutmeg")
+print(f"Updated spices set: {spices}")
+print(f"ID of updated spices:", id(spices))
